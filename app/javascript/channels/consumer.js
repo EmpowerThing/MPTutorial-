4ed597f56047c1090f5f454fdffc5b5521be3d14
EmpowerThing <106834774+EmpowerThing@users.noreply.github.com>
2020-10-26 22:20:06 +0000

    Add another example

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,2 +1,6 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate
+// You can generate new channels where WebSocket features live using the `rails generate channel` command.
+
+import { createConsumer } from "@rails/actioncable"
+
+export default createConsumer()
