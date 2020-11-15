7910efeb8a79910d69e1529131fca873081c92e0
EmpowerThing <106834774+EmpowerThing@users.noreply.github.com>
2020-11-15 12:41:32 +0000

    EOF

---
+++ config/application.rb
@@ -0,0 +1,9 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+mod
