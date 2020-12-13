32c41776ba03b544056e0a07a80720f0959b5b6c
EmpowerThing <106834774+EmpowerThing@users.noreply.github.com>
2020-12-13 09:23:01 +0000

    new config file

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,4 +26,19 @@ Rails.application.configure do
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispat
+  config.action_dispatch.show_exceptions = false
+
+  # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+
+  # Store uploaded files on the local file system in a temporary directory.
+  config.active_storage.service = :test
+
+  config.action_mailer.perform_caching = false
+
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+
+  # Print deprecat
