c233caa29a370d506e3662465cdc4dc4f87d9e13
EmpowerThing <106834774+EmpowerThing@users.noreply.github.com>
2020-12-20 09:28:48 +0000

    Preparing for release

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,4 +10,7 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
-# These infle
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
