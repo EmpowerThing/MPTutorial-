26dd7bffc631f941f3dab460c551f92e89ea54b3
EmpowerThing <106834774+EmpowerThing@users.noreply.github.com>
2021-01-25 15:52:20 +0000

    devres: fix a for loop bounds check

--- db/schema.rb
+++ db/schema.rb
@@ -3,4 +3,17 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_07_14_180600) do
+
+  create_table "coins", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
+    t.string "url_image"
+    t.datetime "created_at", precisio
